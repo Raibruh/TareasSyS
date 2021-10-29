a383eac8db58577121d635ef35413d793995996f
Raibruh <80918062+Raibruh@users.noreply.github.com>
2021-10-29 20:54:44 -0300

    Update p5.py

--- p5.py
+++ p5.py
@@ -8,3 +8,14 @@ from simetra import Simetra
 
 def intsinc(f, factor):
     N = len(f)
+    muestreo = []
+    m_sinc = np.sinc(factor * f)
+    for i in range(N):
+        muestreo.append(np.convolve(f[i:N], m_sinc))
+
+    g = []
+    for lista in muestreo:
+        for elem in lista:
+            g.append(elem)
+
+    return g
